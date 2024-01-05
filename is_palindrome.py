eea8b9ee59ace699e8aeaa62e08816201134a06e
Nokdujon <nokdujon@outlook.com>
2024-01-05 17:16:13 +0700

    + use recursion

--- is_palindrome.py
+++ is_palindrome.py
@@ -6,9 +6,7 @@ def check_for_palindrome_using_loop(word: str) -> bool:
         length = len(word)
 
         match length:
-            case 0:
-                return False
-            case 1:
+            case 0 | 1:
                 return True
             case _:
                 for id in range(math.floor(length / 2)):
@@ -21,4 +19,22 @@ def check_for_palindrome_using_loop(word: str) -> bool:
         print(e)
 
 
+def check_for_palindrome_using_recursion(word: str) -> bool:
+    try:
+        length = len(word)
+
+        match length:
+            case 0 | 1:
+                return True
+            case _:
+                if word[0] == word[length - 1]:
+                    return check_for_palindrome_using_recursion(word[1 : length - 1])
+
+        return False
+
+    except Exception as e:
+        print(e)
+
+
 print("True" if check_for_palindrome_using_loop("racecar") else "False")
+print("True" if check_for_palindrome_using_recursion("racecar") else "False")
